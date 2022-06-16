b0229769a4234de5d4ff5540e1ab5e62a1e30d99
clerver-cirno <553580053@qq.com>
2022-06-16 11:09:51 +0800

    day1

---
+++ day1.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import time
+driver=webdriver.Chrome()
+
+driver.get("D:\作业\软件质量保证与测试实习\Web自动化测试\page\注册A.html")
+
+time.sleep(3)
+driver.find_element_by_name("passwordA").send_keys("123456")
+time.sleep(3)
+
+driver.find_element_by_xpath("//form/p/button").click()
+time.sleep(3)
+
+driver.quit()
